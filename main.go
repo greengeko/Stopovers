07e8c2eaf5ffa0ec1ec9d16e8aaa429e6e6b1875
ReicelaMackevica <mackevicar@gmail.com>
2022-08-03 02:35:21 +0200

    tests and writing on file

--- main.go
+++ main.go
@@ -17,6 +17,27 @@ var avoid0 []string
 
 func main() {
 	fmt.Println("Hello, World!")
+	parametersTest := kiwi.Parameters{
+		//FlyFrom: "1.35-103.81-250km",
+		FlyFrom: "SIN",
+		FlyTo:   "ATH",
+		Partner: "gekostopovers",
+		//PriceFrom: 100,
+		//OneForCity:    true,
+		Limit:         1000,
+		DepartureFrom: "2022-09-05T00:00",
+		DepartureTo:   "2022-09-15T00:00",
+		//ArrivalFrom
+		//ArrivalTo
+	}
+	resp, _ := k.GetFlights(&parametersTest)
+
+	flight2 := kiwi.Flight{}
+	for i := 0; i < len(resp.Flights); i++ {
+		fmt.Print(flight2.FlyFrom)
+		fmt.Print(flight2.FlyTo)
+		fmt.Println(flight2.Price)
+	}
 	//k := kiwi.New()
 
 	params := kiwi.Parameters{
@@ -32,8 +53,9 @@ func main() {
 		//ArrivalTo
 	}
 	avoid0 = append(avoid0, params.FlyFrom)
-	searchFlights(mother, params, 0, avoid0)
+	var tail int = searchFlights(mother, params, 0, avoid0)
 	//fmt.Println(options)
+	fmt.Println(tail)
 	for y := 0; y < len(options); y++ {
 		/*fmt.Println("prezzo:")
 		fmt.Println(options[y].Price)*/
@@ -48,7 +70,8 @@ func main() {
 
 }
 
-func searchFlights(n Node, params kiwi.Parameters, price float64, avoid []string) {
+func searchFlights(n Node, parameters kiwi.Parameters, price float64, avoid []string) int {
+	var num = 0
 	var ok = true
 	price = price + n.data.Price
 	if price > 400 && n.parent != &mother && &n != &mother && price-n.data.Price != 0 && price-n.data.Price < 400 {
@@ -58,7 +81,7 @@ func searchFlights(n Node, params kiwi.Parameters, price float64, avoid []string
 			avoid = append(avoid, n.data.Route[a].FlyTo)
 		}
 		for j := 0; j < len(avoid); j++ {
-			if avoid[j] == n.data.FlyTo && n.data.FlyTo != "" {
+			if avoid[j] == n.data.FlyTo && n.data.FlyFrom != "SIN" {
 				ok = false
 				if price < 400 {
 					createFlight(n, price)
@@ -70,13 +93,14 @@ func searchFlights(n Node, params kiwi.Parameters, price float64, avoid []string
 			createFlight(n, price) //da nodi risale per formare una lista e metterla in options
 		} else if ok {
 			//createFlight(n)
-			resp, _ := k.GetFlights(&params)
+			resp, _ := k.GetFlights(&parameters)
 			flight := kiwi.Flight{}
 			for i := 0; i < len(resp.Flights); i++ {
 				flight = resp.Flights[i]
 				//fmt.Println(flight.FlyFrom + "to" + flight.FlyTo + "for" + strconv.Itoa(int(flight.Price)))
 				var flightNode = Node{data: flight, parent: &n}
-				n.children.PushFront(flightNode)
+				n.children = append(n.children, &flightNode)
+				//printFile(flightNode)
 
 				unixTimeUTCTO := time.Unix(int64(flight.ArrivalTimeUTC), 0).AddDate(0, 0, 4)
 				unixTimeUTCFROM := time.Unix(int64(flight.ArrivalTimeUTC), 0).AddDate(0, 0, 1) //gives unix time stamp in utc
@@ -89,26 +113,38 @@ func searchFlights(n Node, params kiwi.Parameters, price float64, avoid []string
 				flightParams := kiwi.Parameters{FlyFrom: flight.FlyTo, Partner: "gekostopovers", Limit: 1000,
 					OneForCity: true, DepartureFrom: departureFrom, DepartureTo: departureTo} //aggiungere param temporali
 				//fmt.Println("da" + flight.FlyTo)
-				searchFlights(flightNode, flightParams, price, avoid)
+				num = 1 + searchFlights(flightNode, flightParams, price, avoid)
+
 			}
+
 		}
 	}
+	return num
 }
 
 func createFlight(n Node, price float64) {
 	/*var flight = kiwi.Flight{}
 	flight.Price = price*/
-	for n.parent != &mother && n.parent != nil {
+	printFile(n)
+	for n.parent != nil {
 		print(n)
+
+		//createFlight(*n.parent, price)
+
 		/*for i := 0; i < len(n.data.Route); i++ {
 			flight.Route = append(flight.Route, n.data.Route[i])
 
 		}
 		var route = kiwi.Route{FlyTo: "---"}
 		flight.Route = append(flight.Route, route)*/
-		n = *n.parent
+		//var parent Node = n.Parent()
+		n.data = (*n.parent).data
+		n.parent = (*n.parent).parent
+		//n.children = (*n.parent).children
+
 	}
 	fmt.Println("flight added ")
+
 	/*fmt.Println(flight.Price)
 	for i := 0; i < len(flight.Route); i++ {
 		fmt.Print(flight.Route[i].FlyTo)
@@ -124,4 +160,5 @@ func createFlight(n Node, price float64) {
 		fmt.Println(options[0].Route[i].FlyFrom)
 		fmt.Println(options[0].Route[i].FlyTo)
 	}*/
+
 }
--- tree.go
+++ tree.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"container/list"
+	"bufio"
 	"fmt"
 	"github.com/greengeko/kiwi-go"
+	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
 type Node struct {
 	data     kiwi.Flight
 	parent   *Node
-	children list.List
+	children []*Node
 }
 
 func print(node Node) {
 	for i := 0; i < len(node.data.Route); i++ {
-		fmt.Print("Flight from " + node.data.Route[i].FlyFrom + "to " + node.data.Route[i].FlyTo + " and flight number: ")
+		fmt.Print("Flight from " + node.data.Route[i].FlyFrom + " to " + node.data.Route[i].FlyTo + " and flight number: ")
 		fmt.Print(node.data.Route[i].OperatingFlightNo)
 
 	}
@@ -25,4 +28,58 @@ func print(node Node) {
 	fmt.Print("for the price: ")
 	fmt.Print(node.data.Price)
 
+}
+
+func printFile(node Node) {
+	var price int = 0
+	//var filename = RandStringBytes(10)
+	var filename string = node.data.FlyFrom + node.data.FlyTo
+	//var filenameNew string = filename
+	nuovofile, errore := os.Create(filename + ".txt")
+	if errore == nil {
+		fmt.Println("File creato: ", nuovofile.Name())
+	} else {
+		fmt.Println("Errore: ", errore)
+		panic(errore)
+	}
+	myWriter := bufio.NewWriter(nuovofile)
+	myWriter.WriteString("\nciao a tutti")
+	for node.parent != nil {
+
+		for i := 0; i < len(node.data.Route); i++ {
+			myWriter.WriteString("Flight from " + node.data.Route[i].FlyFrom + "to " + node.data.Route[i].FlyTo + " and flight number: ")
+			myWriter.WriteString(node.data.Route[i].OperatingFlightNo)
+			//filenameNew = filenameNew + node.data.FlyTo
+
+		}
+		unixTimeUTC := time.Unix(int64(node.data.DepartureTimeUTC), 0) //gives unix time stamp in utc
+		unitTimeInRFC3339 := unixTimeUTC.Format(time.RFC3339)
+		myWriter.WriteString("departure on: " + unitTimeInRFC3339)
+		myWriter.WriteString("for the price: ")
+		myWriter.WriteString(strconv.Itoa(int(node.data.Price)))
+		price = price + int(node.data.Price)
+		node.data = (*node.parent).data
+		node.parent = (*node.parent).parent
+
+	}
+	myWriter.WriteString("ARRIVO a " + node.data.FlyTo + "with price: " + strconv.Itoa(price))
+	/*e := os.Rename(filename+".txt", filenameNew+".txt")
+	if e != nil {
+		log.Fatal(e)
+	}*/
+	myWriter.Flush()
+	nuovofile.Close()
+	fmt.Println("File popolato")
+	//node = *node.parent
+
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
